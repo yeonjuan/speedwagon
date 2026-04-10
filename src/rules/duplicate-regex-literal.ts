d0aba5684ec1130709f55dc3731b0526b2c14a06
YeonJuan <yeonjuan93@naver.com>
2026-04-10 23:08:38 +0900

    fix

---
+++ src/rules/duplicate-regex-literal.ts
@@ -0,0 +1,7 @@
+import type { Rule } from "./types";
+
+export const duplicateRegexLiteral: Rule = {
+  name: "duplicate-regex-literal",
+  description: "TODO",
+  check() {},
+};
---
+++ src/rules/types.ts
@@ -0,0 +1,5 @@
+export interface Rule {
+  name: string;
+  description: string;
+  check(): void;
+}
