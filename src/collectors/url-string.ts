47364c22f552363006e7a6e8709cf42c59331e57
YeonJuan <yeonjuan93@naver.com>
2026-04-18 15:20:06 +0900

    Update url-string.ts

--- src/collectors/url-string.ts
+++ src/collectors/url-string.ts
@@ -6,7 +6,7 @@ function isUrlString(value: string) {
   return /^https?:\/\//.test(value);
 }
 
-function normalizeStringLiteral(node: StringLiteral | TemplateLiteral): string {
+function normalize(node: StringLiteral | TemplateLiteral): string {
   if (isStringLiteral(node)) return node.value;
   return node.quasis[0].value.cooked || "";
 }
@@ -17,7 +17,7 @@ function truncate(value: string) {
   return value.length > MAX_LENGTH ? value.slice(0, MAX_LENGTH) + "..." : value;
 }
 
-function printStringLiteral(node: StringLiteral | TemplateLiteral) {
+function print(node: StringLiteral | TemplateLiteral) {
   if (isStringLiteral(node)) return truncate(node.value);
   return truncate(node.quasis[0]?.value.cooked || "");
 }
@@ -25,36 +25,33 @@ function printStringLiteral(node: StringLiteral | TemplateLiteral) {
 export const urlString: Collector = {
   id: "url-string",
   createJSVisitor(context) {
+    function report(node: StringLiteral | TemplateLiteral) {
+      const key = normalize(node);
+      const displayName = print(node);
+      context.add({
+        key,
+        displayName,
+        location: {
+          start: getPosition(context.code, node.start),
+          end: getPosition(context.code, node.end),
+        },
+      });
+    }
+
     return {
       Literal(node) {
         if (!isStringLiteral(node)) return;
         if (!isUrlString(node.value)) return;
-        const key = normalizeStringLiteral(node);
-        const displayName = printStringLiteral(node);
-        context.add({
-          key,
-          displayName,
-          location: {
-            start: getPosition(context.code, node.start),
-            end: getPosition(context.code, node.end),
-          },
-        });
+
+        report(node);
       },
       TemplateLiteral(node) {
         if (node.expressions.length > 0) return;
         const value = node.quasis[0]?.value.cooked;
         if (!value) return;
         if (!isUrlString(value)) return;
-        const key = normalizeStringLiteral(node);
-        const displayName = printStringLiteral(node);
-        context.add({
-          key,
-          displayName,
-          location: {
-            start: getPosition(context.code, node.start),
-            end: getPosition(context.code, node.end),
-          },
-        });
+
+        report(node);
       },
     };
   },
